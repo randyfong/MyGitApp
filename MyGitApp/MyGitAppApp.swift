cdd33f716798b2d301d29dc287c4a743df4c1d79
Randy Fong <randyfong@aol.com>
2023-03-14 12:31:11 -0700

    Initial Commit

---
+++ MyGitApp/MyGitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGitAppApp.swift
+//  MyGitApp
+//
+//  Created by Randy Fong on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MyGitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
