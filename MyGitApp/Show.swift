6c86bc58f731c28c1e4ef2982eaf15c19df60e98
Randy Fong <randyfong@aol.com>
2023-03-14 13:04:46 -0700

    Change test to "Not My dog"

---
+++ MyGitApp/Show.swift
@@ -0,0 +1,14 @@
+//
+//  Show.swift
+//  MyGitApp
+//
+//  Created by Randy Fong on 3/14/23.
+//
+
+import Foundation
+
+class Show {
+    static func myDogHas(_ text: String) -> String {
+        "My dog has: " + text
+    }
+}
--- MyGitAppTests/MyGitAppTests.swift
+++ MyGitAppTests/MyGitAppTests.swift
@@ -10,20 +10,10 @@ import XCTest
 
 final class MyGitAppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let text = "fleas"
+        XCTAssertTrue(Show.myDogHas(text) == "Not My dog has: \(text)", "'myDogHas' function not returning correct string")
     }
 
     func testPerformanceExample() throws {
