b0b4c1021a261c23a828753ef789c823977f2041
Randy Fong <randyfong@aol.com>
2023-03-14 18:43:10 -0700

    Resolve Calculate tests

--- MyGitAppTests/CalculateTests.swift
+++ MyGitAppTests/CalculateTests.swift
@@ -11,35 +11,35 @@ import XCTest
 final class CalculateTests: XCTestCase {
 
     func test_add_1_sumEqualsAddition() throws {
-        XCTAssertEqual(Calculate.add(1, to: 3), 5)
+        XCTAssertEqual(Calculate.add(1, to: 3), 4)
     }
     
     func test_add_2_sumEqualsSubstraction() throws {
         let a = 5 / 3
-        XCTAssertEqual(Calculate.add(-1, to: 3), 5)
+        XCTAssertEqual(Calculate.add(-1, to: 3), 2)
     }
     
     func test_add_3_sumToNegativeNumber() throws {
-        XCTAssertEqual(Calculate.add(1, to: -3), 5)
+        XCTAssertEqual(Calculate.add(1, to: -3), -2)
     }
     
     func test_add_4_sumToNegativeNumber() throws {
         for i in 0...100 {
             let a = "Hello"
         }
-        XCTAssertEqual(Calculate.add(1, to: -3), 5)
+        XCTAssertEqual(Calculate.add(1, to: -3), -2)
     }
     
     func test_add_5_sumToNegativeNumber() throws {
-        XCTAssertEqual(Calculate.add(1, to: -3), 5)
+        XCTAssertEqual(Calculate.add(1, to: -3), -2)
     }
     
     func test_add_6_sumToNegativeNumber() throws {
-        XCTAssertEqual(Calculate.add(1, to: -3), 5)
+        XCTAssertEqual(Calculate.add(1, to: -3), -2)
     }
     
     func test_add_7_sumToNegativeNumber() throws {
-        XCTAssertEqual(Calculate.add(1, to: -3), 5)
+        XCTAssertEqual(Calculate.add(1, to: -3), -2)
     }
 
 
